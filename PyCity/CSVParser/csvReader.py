bb79a900f8fe640a96d0e24bf629f60d197700ac
phatthanapong <pthongth@celestica.com>
2016-09-09 11:57:45 +0700

    plot

--- PyCity/CSVParser/csvReader.py
+++ PyCity/CSVParser/csvReader.py
@@ -1,5 +1,13 @@
 import csv
+import numpy as np
+import plotly.graph_objs as go
+import plotly.plotly as py
+import plotly
 
+plotly.offline.init_notebook_mode()
+# plotly.offline.plot()
+# plotly.offline.iplot()
+# plotly.tools.set_credentials_file(username='aetdevinz', api_key='p5gx4csvk1')
 
 Car = []
 each_pos = []
@@ -17,9 +25,47 @@ with open('/home/phatthanapong/Desktop/City/PyCity/Data/Car.csv') as csvfile:
         else:
             temp = {k: row[k] for k in row.keys() & {'latitude', 'longitude', 'speed', 'bering', 'time', 'accuracy'}}
             each_pos.append(temp)
+# list_speed =[]
+# for car in Car:
+#     speed = [float(s['speed']) for s in car]
+#     list_speed.append(speed)
 
-    print(Car[0][0])
+list_speed = [float(d['speed']) for d in Car[0]]
+
+list_range = list(range(1, 100))
+trace1 = go.Scatter(
+    y = list_speed,
+    mode='markers',
+    marker=dict(
+        size='8',
+        color='rgba(255, 0, 0, .8)',
+        colorscale='Viridis'
+    )
+)
+data = [trace1]
+
+# py.iplot(data, filename='scatter-plot')
+plotly.offline.plot(data, filename='scatter-plot')
 
 
 
 
+# trace1 = go.Scatter(
+#     y = np.random.randn(500),
+#     mode='markers',
+#     marker=dict(
+#         size='16',
+#         color = np.random.randn(500), #set color equal to a variable
+#         colorscale='Viridis',
+#         showscale=True
+#     )
+# )
+# data = [trace1]
+# plotly.offline.plot(data, filename='scatter-plot-with-colorscale')
+# py.iplot(data, filename='scatter-plot-with-colorscale')
+
+
+# plotly.offline.plot({
+#     "data": [go.Scatter(x=[1, 2, 3, 4], y=[4, 3, 2, 1])],
+#     "layout": go.Layout(title="hello world")
+# })
