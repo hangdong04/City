0ff5918862e2a3e8e9dc33244730662e505dd2ee
phatthanapong <pthongth@celestica.com>
2016-10-06 16:50:22 +0700

    move preprocessing to function

--- PyCity/CSVParser/Helper.py
+++ PyCity/CSVParser/Helper.py
@@ -23,4 +23,66 @@ class Helper:
                     each_pos.append(temp)
         return data
 
+    def preprocess(self, obj_in):
+        data = []
+        data_list = dict()
+        speed = []
+        accuracy = []
+        time = []
+        temp_t = None
+        temp_a = None
+        temp_s = None
+        for item in obj_in:
+            found = 0
+            for idx, val in enumerate(item):
+                t = val['time']
+                a = val['accuracy']
+                s = val['speed']
+                if float(s) == 0.0:
+                    speed.append(s)
+                    time.append(t)
+                    accuracy.append(a)
+                    if found == 1:
+                        if idx != len(item) - 1:
+                            del speed[len(speed) - 1]
+                            del time[len(time) - 1]
+                            del accuracy[len(accuracy) - 1]
+                    found = 1
+                else:
+                    if found == 1:
+                        speed.append(temp_s)
+                        time.append(temp_t)
+                        accuracy.append(temp_a)
+                    found = 0
+                    speed.append(s)
+                    time.append(t)
+                    accuracy.append(a)
+                temp_s = s
+                temp_t = t
+                temp_a = a
+
+            for idx, val in enumerate(time):
+                if idx != 0:
+                    if temp_t > val:
+                        del speed[idx]
+                        del time[idx]
+                        del accuracy[idx]
+                    else:
+                        temp_t = val
+                else:
+                    temp_t = val
+            for idx, val in enumerate(accuracy):
+                if float(val) > 200:
+                    del speed[idx]
+                    del time[idx]
+                    del accuracy[idx]
+
+            data_list['speed'] = speed
+            data_list['time'] = time
+            data_list['accuracy'] = accuracy
+            data.append(data_list)
+            speed = []
+            time = []
+            accuracy = []
+        return data
 
--- PyCity/CSVParser/test.py
+++ PyCity/CSVParser/test.py
@@ -7,69 +7,8 @@ bike_path = '/home/phatthanapong/Desktop/City/PyCity/Data/Bicycle.csv'
 run_path = '/home/phatthanapong/Desktop/City/PyCity/Data/Run.csv'
 walk_path = '/home/phatthanapong/Desktop/City/PyCity/Data/Walk.csv'
 
-data = []
 Helper = Helper()
 Car = Helper.reader(car_path)
-car_list =dict()
-speed = []
-accuracy = []
-time = []
-zero = []
-inn = []
-temp = None
-temp_t = None
-temp_a = None
-temp_s = None
-for car in Car:
-    found = 0
-    for idx, val in enumerate(car):
-        t = val['time']
-        a = val['accuracy']
-        s = val['speed']
-        if float(s) == 0.0:
-            speed.append(s)
-            time.append(t)
-            accuracy.append(a)
-            if found == 1:
-                if idx != len(car)-1:
-                    del speed[len(speed)-1]
-                    del time[len(time) - 1]
-                    del accuracy[len(accuracy) - 1]
-            found = 1
-        else:
-            if found == 1:
-                speed.append(temp_s)
-                time.append(temp_t)
-                accuracy.append(temp_a)
-            found = 0
-            speed.append(s)
-            time.append(t)
-            accuracy.append(a)
-        temp_s = s
-        temp_t = t
-        temp_a = a
+data = Helper.preprocess(Car)
+print(len(data))
 
-    for idx, val in enumerate(time):
-        if idx != 0:
-            if temp_t > val:
-                del speed[idx]
-                del time[idx]
-                del accuracy[idx]
-            else:
-                temp_t = val
-        else:
-            temp_t = val
-    for idx, val in enumerate(accuracy):
-        if float(val) > 200:
-            del speed[idx]
-            del time[idx]
-            del accuracy[idx]
-
-    car_list['speed'] = speed
-    car_list['time'] = time
-    car_list['accuracy'] = accuracy
-    data.append(car_list)
-    speed = []
-    time = []
-    accuracy = []
-print(data)
