3fc4a33c50ee2b281c106fa5c5e25052a5cd2f28
hangdong04 <aet121@gmail.com>
2016-11-11 10:23:41 +0700

    last

--- PyCity/CSVParser/test.py
+++ PyCity/CSVParser/test.py
@@ -5,17 +5,17 @@ from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 # Wndows path
-# car_path = 'C:\\Users\Phatthanaphong\Desktop\City\PyCity\Data\Car.csv'
-# motor_path = 'C:\\Users\Phatthanaphong\Desktop\City\PyCity\Data\Motorcycle.csv'
-# bike_path = 'C:\\Users\Phatthanaphong\Desktop\City\PyCity\Data\Bicycle.csv'
-# run_path = 'C:\\Users\Phatthanaphong\Desktop\City\PyCity\Data\Run.csv'
-# walk_path = 'C:\\Users\Phatthanaphong\Desktop\City\PyCity\Data\Walk.csv'
+car_path = 'C:\\Users\Phatthanaphong\Desktop\City\PyCity\Data\Car.csv'
+motor_path = 'C:\\Users\Phatthanaphong\Desktop\City\PyCity\Data\Motorcycle.csv'
+bike_path = 'C:\\Users\Phatthanaphong\Desktop\City\PyCity\Data\Bicycle.csv'
+run_path = 'C:\\Users\Phatthanaphong\Desktop\City\PyCity\Data\Run.csv'
+walk_path = 'C:\\Users\Phatthanaphong\Desktop\City\PyCity\Data\Walk.csv'
 # Linux path
-car_path = '/home/phatthanapong/Desktop/City/PyCity/Data/Car.csv'
-motor_path = '/home/phatthanapong/Desktop/City/PyCity/Data/Motorcycle.csv'
-bike_path = '/home/phatthanapong/Desktop/City/PyCity/Data/Bicycle.csv'
-run_path = '/home/phatthanapong/Desktop/City/PyCity/Data/Run.csv'
-walk_path = '/home/phatthanapong/Desktop/City/PyCity/Data/Walk.csv'
+# car_path = '/home/phatthanapong/Desktop/City/PyCity/Data/Car.csv'
+# motor_path = '/home/phatthanapong/Desktop/City/PyCity/Data/Motorcycle.csv'
+# bike_path = '/home/phatthanapong/Desktop/City/PyCity/Data/Bicycle.csv'
+# run_path = '/home/phatthanapong/Desktop/City/PyCity/Data/Run.csv'
+# walk_path = '/home/phatthanapong/Desktop/City/PyCity/Data/Walk.csv'
 
 Helper = Helper()
 
